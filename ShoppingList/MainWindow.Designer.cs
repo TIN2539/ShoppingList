9f2394c4561df9255452dc945e0e46c45ade538c
Алексей Бершаский <alesha60@hotmail.com>
2018-05-28 23:59:34 +0300

    Close #1

--- ShoppingList/MainWindow.Designer.cs
+++ ShoppingList/MainWindow.Designer.cs
@@ -73,6 +73,9 @@
 			// 
 			// productCheckedListBox
 			// 
+			this.productCheckedListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.productCheckedListBox.Items.AddRange(new object[] {
             "Avocado",
             "Chicken",
